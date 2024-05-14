0ee7d5794b0863758d0b8e973681da0a9f07d488
Mojammel Noman <molla.noman23@gmail.com>
2024-05-14 15:58:19 +0600

    added vercel.json file for deployment

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       "https://assignment11-c9a59.web.app",
       "https://assignment11-c9a59.firebaseapp.com",
     ],
@@ -34,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const database = client.db("HelpConnect");
     const allPost = database.collection("needVolunteer");
     const reviews = database.collection("reviews");
@@ -170,7 +170,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
